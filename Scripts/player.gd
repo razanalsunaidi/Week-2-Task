7970c5ce84880cd24e473f01b0e74818476b9b9d
razanalsunaidi <razanalsunedi@gmail.com>
2025-02-26 16:28:27 +0300

    Donee

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,31 @@
+
+
 extends CharacterBody2D
-
-@export var speed : float = 400
-@export var gravity : float = 500
-@export var jump_force : float = -400
-
-# Called when the node enters the scene tree for the first time.
+@onready var color_rect : ColorRect = $ColorRect
+@export var speed : float = 10
+@export var gravity : float = 300
+@export var jump : float = -300
+#Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	
+#Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	var input_direction : float = Input.get_axis("player_left", "player_right")
-	velocity.x = input_direction * speed
-
-	if not is_on_floor():
+	
+	if input_direction:
+		color_rect.color = Color.GREEN
+	else:
+		color_rect.color = Color.WHITE
+			
+	velocity.x += input_direction * speed 
+	
+	if ! is_on_floor():
 		velocity.y += gravity * delta
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("player_jump"):
-			velocity.y = jump_force
-	
+			velocity.y = jump
+		
 	move_and_slide()
